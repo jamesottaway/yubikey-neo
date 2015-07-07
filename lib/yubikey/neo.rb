2970fdf65932230b55b30496c50485680c1e8bae
James Ottaway <git@james.ottaway.io>
2015-07-07 20:41:43 +1000

    bundle gem yubikey-neo

---
+++ lib/yubikey/neo.rb
@@ -0,0 +1,7 @@
+require "yubikey/neo/version"
+
+module Yubikey
+  module Neo
+    # Your code goes here...
+  end
+end
---
+++ lib/yubikey/neo/version.rb
@@ -0,0 +1,5 @@
+module Yubikey
+  module Neo
+    VERSION = "0.1.0"
+  end
+end
