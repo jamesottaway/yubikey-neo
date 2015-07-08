630116156fdc1ea0201576b9bafab9c79c468028
James Ottaway <git@james.ottaway.io>
2015-07-08 20:22:45 +1000

    Talk to the Neo via the `smartcard` gem

--- lib/yubikey/neo.rb
+++ lib/yubikey/neo.rb
@@ -1,4 +1,31 @@
+require 'smartcard'
+
 module Yubikey
   class Neo
+    def initialize(name: 'Yubico Yubikey NEO OTP+CCID')
+      @name = name
+    end
+
+    def tap
+      Context.tap do |context|
+        begin
+          card = context.card(@name)
+          yield card
+        ensure
+          card.disconnect unless card.nil?
+        end
+      end
+    end
+
+    module Context
+      extend self
+
+      def tap
+        context = Smartcard::PCSC::Context.new
+        yield context
+      ensure
+        context.release
+      end
+    end
   end
 end
--- yubikey-neo.gemspec
+++ yubikey-neo.gemspec
@@ -15,4 +15,6 @@ Gem::Specification.new do |spec|
   spec.bindir        = 'exe'
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ['lib']
+
+  spec.add_dependency 'smartcard', '~> 0.5.5'
 end
